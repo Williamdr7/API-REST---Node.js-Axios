5a22879c6abe46d577b233765a361f8e93823e07
William <williamduarte499@gmail.com>
2020-07-21 13:56:22 -0300

    Interação completa na API com Axios

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const { json } = require("body-parser")
+const cors = require("cors")
 
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
